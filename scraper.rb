32472761a46248b965cc5bc35408aac93b9f5bfc
Tony Bowden <tony@tmtm.com>
2020-07-26 20:07:44 +0100

    Make scraper work

--- scraper.rb
+++ scraper.rb
@@ -7,11 +7,13 @@ require 'scraped'
 require 'wikidata_ids_decorator'
 
 require_relative 'lib/partial_date'
+require_relative 'lib/remove_notes'
 require_relative 'lib/unspan_all_tables'
 require_relative 'lib/wikipedia_table_row'
 
 # The Wikipedia page with a list of officeholders
 class ListPage < Scraped::HTML
+  decorator RemoveNotes
   decorator WikidataIdsDecorator::Links
   decorator UnspanAllTables
 
@@ -24,13 +26,17 @@ class ListPage < Scraped::HTML
   def list
     noko.xpath('.//table[.//th[contains(
       translate(., "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"),
-    "portrait")]]').last
+    "took office")]]')
   end
 end
 
 
 # Each officeholder in the list
 class HolderItem < WikipediaTableRow
+  field :ordinal do
+    tds[0].text.to_i
+  end
+
   field :id do
     wikidata_ids_in(tds[2]).first
   end
@@ -40,13 +46,13 @@ class HolderItem < WikipediaTableRow
   end
 
   field :start_date do
-    Date.parse tds[4].text rescue binding.pry
+    Date.parse tds[3].text rescue nil
   end
 
   field :end_date do
-    return if tds[5].text.include? 'Incumbent'
+    return if tds[4].text.include? 'Incumbent'
 
-    Date.parse tds[5].text
+    Date.parse tds[4].text rescue nil
   end
 
   field :replaces do
@@ -56,7 +62,7 @@ class HolderItem < WikipediaTableRow
   end
 
   def empty?
-    tds[0].css('img/@alt').text.include? 'Flag'
+    table_headings.include? 'Regent'
   end
 end
 
@@ -64,7 +70,7 @@ url = ARGV.first || abort("Usage: #{$0} <url to scrape>")
 data = Scraped::Scraper.new(url => ListPage).scraper.officeholders
 
 data.each_cons(2) do |prev, cur|
-  cur[:replaces] = prev[:id]
+  cur[:replaces] = prev[:id] if 
   prev[:replaced_by] = cur[:id]
 end
 
